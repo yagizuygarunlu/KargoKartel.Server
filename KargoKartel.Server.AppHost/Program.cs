10b78fd2952c0c6adce825d21bd4a580d3116b7f
yagizugarunlu <unluyagizuygar@gmail.com>
2025-07-10 22:29:59 +0300

    update

--- KargoKartel.Server.AppHost/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-// Reference the WebAPI project
-builder.AddProject<Projects.KargoKartel_Server_WebAPI>("webapi");
-
-builder.Build().Run();
